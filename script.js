c56a1c88ceaaf84c90a5865883d679c997abbc91
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-27 20:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const slider = document.querySelector('.items');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+slider.addEventListener('mousedown', (event) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = event.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    console.log('mousedown', { startX, scrollLeft });
+});
 
-    slider.addEventListener('mousedown', (event) => {
-        isDown = true;
-        slider.classList.add('active')
-        startX = event.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
-    
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.classList.remove('active')
-    });
+slider.addEventListener('mousemove', (event) => {
+    if (!isDown) return;
+    event.preventDefault();
+    const x = event.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2;
+    slider.scrollLeft = scrollLeft - walk;
+    console.log('mousemove', { x, walk, scrollLeft: slider.scrollLeft });
+});
 
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.classList.remove('active')
-    });
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    console.log('mouseup');
+});
 
-    slider.addEventListener('mousemove', (event) => {
-        if (!isDown) return;
-        event.preventDefault();
-        const x = event.pageX - slider.offsetLeft;
-        const walk = (x - startX)*2;
-        slider.scrollLeft = scrollLeft - walk;
-    });
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    console.log('mouseleave');
+});
